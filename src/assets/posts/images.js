19ca4d53aa67f2f4bef4d8c991bb87af7f773529
dgbragas <dgbragas.dev@outlook.com>
2020-08-09 18:57:08 -0300

    Adjusting mocked posts data

--- src/assets/posts/images.js
+++ src/assets/posts/images.js
@@ -1,60 +1,60 @@
-import Post01_Image01 from './posts/01-post/image-01.png';
-import Post01_Image02 from './posts/01-post/image-02.png';
-import Post01_Image03 from './posts/01-post/image-03.png';
+import Post01_Image01 from './01-post/image-01.png';
+import Post01_Image02 from './01-post/image-02.png';
+import Post01_Image03 from './01-post/image-03.png';
 
-import Post02_Image01 from './posts/02-post/image-01.png';
-import Post02_Image02 from './posts/02-post/image-02.png';
-import Post02_Image03 from './posts/02-post/image-03.png';
-import Post02_Image04 from './posts/02-post/image-04.png';
-import Post02_Image05 from './posts/02-post/image-05.png';
+import Post02_Image01 from './02-post/image-01.png';
+import Post02_Image02 from './02-post/image-02.png';
+import Post02_Image03 from './02-post/image-03.png';
+import Post02_Image04 from './02-post/image-04.png';
+import Post02_Image05 from './02-post/image-05.png';
 
-import Post03_Image01 from './posts/03-post/image-01.png';
-import Post03_Image02 from './posts/03-post/image-02.png';
+import Post03_Image01 from './03-post/image-01.png';
+import Post03_Image02 from './03-post/image-02.png';
 
-import Post04_Image01 from './posts/04-post/image-01.png';
+import Post04_Image01 from './04-post/image-01.png';
 
-import Post05_Image01 from './posts/05-post/image-01.png';
-import Post05_Image02 from './posts/05-post/image-02.png';
-import Post05_Image03 from './posts/05-post/image-03.png';
-import Post05_Image04 from './posts/05-post/image-04.png';
+import Post05_Image01 from './05-post/image-01.png';
+import Post05_Image02 from './05-post/image-02.png';
+import Post05_Image03 from './05-post/image-03.png';
+import Post05_Image04 from './05-post/image-04.png';
 
-import Post06_Image01 from './posts/06-post/image-01.png';
-import Post06_Image02 from './posts/06-post/image-02.png';
-import Post06_Image03 from './posts/06-post/image-03.png';
+import Post06_Image01 from './06-post/image-01.png';
+import Post06_Image02 from './06-post/image-02.png';
+import Post06_Image03 from './06-post/image-03.png';
 
-import Post07_Image01 from './posts/07-post/image-01.png';
-import Post07_Image02 from './posts/07-post/image-02.png';
-import Post07_Image03 from './posts/07-post/image-03.png';
+import Post07_Image01 from './07-post/image-01.png';
+import Post07_Image02 from './07-post/image-02.png';
+import Post07_Image03 from './07-post/image-03.png';
 
-import Post08_Image01 from './posts/08-post/image-01.png';
-import Post08_Image02 from './posts/08-post/image-02.png';
-import Post08_Image03 from './posts/08-post/image-03.png';
+import Post08_Image01 from './08-post/image-01.png';
+import Post08_Image02 from './08-post/image-02.png';
+import Post08_Image03 from './08-post/image-03.png';
 
-import Post09_Image01 from './posts/09-post/image-01.png';
-import Post09_Image02 from './posts/09-post/image-02.png';
-import Post09_Image03 from './posts/09-post/image-03.png';
+import Post09_Image01 from './09-post/image-01.png';
+import Post09_Image02 from './09-post/image-02.png';
+import Post09_Image03 from './09-post/image-03.png';
 
-import Post10_Image01 from './posts/10-post/image-01.png';
-import Post10_Image02 from './posts/10-post/image-02.png';
-import Post10_Image03 from './posts/10-post/image-03.png';
+import Post10_Image01 from './10-post/image-01.png';
+import Post10_Image02 from './10-post/image-02.png';
+import Post10_Image03 from './10-post/image-03.png';
 
-import Post11_Image01 from './posts/11-post/image-01.png';
-import Post11_Image02 from './posts/11-post/image-02.png';
+import Post11_Image01 from './11-post/image-01.png';
+import Post11_Image02 from './11-post/image-02.png';
 
-import Post12_Image01 from './posts/12-post/image-01.png';
-import Post12_Image02 from './posts/12-post/image-02.png';
-import Post12_Image03 from './posts/12-post/image-03.png';
-import Post12_Image04 from './posts/12-post/image-04.png';
+import Post12_Image01 from './12-post/image-01.png';
+import Post12_Image02 from './12-post/image-02.png';
+import Post12_Image03 from './12-post/image-03.png';
+import Post12_Image04 from './12-post/image-04.png';
 
-import Post13_Image01 from './posts/13-post/image-01.png';
-import Post13_Image02 from './posts/13-post/image-02.png';
-import Post13_Image03 from './posts/13-post/image-03.png';
-import Post13_Image04 from './posts/13-post/image-04.png';
-import Post13_Image05 from './posts/13-post/image-05.png';
+import Post13_Image01 from './13-post/image-01.png';
+import Post13_Image02 from './13-post/image-02.png';
+import Post13_Image03 from './13-post/image-03.png';
+import Post13_Image04 from './13-post/image-04.png';
+import Post13_Image05 from './13-post/image-05.png';
 
-import Post14_Image01 from './posts/14-post/image-01.png';
-import Post14_Image02 from './posts/14-post/image-02.png';
-import Post14_Image03 from './posts/14-post/image-03.png';
+import Post14_Image01 from './14-post/image-01.png';
+import Post14_Image02 from './14-post/image-02.png';
+import Post14_Image03 from './14-post/image-03.png';
 
 export {
   Post01_Image01,
--- src/assets/posts/posts.data.js
+++ src/assets/posts/posts.data.js
@@ -47,18 +47,18 @@ import {
 
 const posts = [
   {
-    _id: 1,
+    id: 1,
     title: 'Platform Redesign',
     name: 'Semantix Sagitário',
-    descripion:
+    description:
       'O Semantix Sagitário é um produto interno da @semantixbr para o qual fui contratado com o objetivo de: Desenvolver uma nova estrutura visual; Inserir novas funcionalidades; Mapear os próximos fluxos; e Iniciar o Style Guide do sistema.',
     images: [Post01_Image01, Post01_Image02, Post01_Image03],
   },
   {
-    _id: 2,
+    id: 2,
     title: 'Redesign Concept',
     name: 'Portal Aberje',
-    descripion:
+    description:
       'A Associação Brasileira de Comunicação Empresarial, ou Aberje, é uma empresa focada em fortalecer o papel da comunicação nas empresas e instituições através de: Estudos estratégicos; Oferecimento de cursos; Reconhecimento de melhores práticas entre outros.',
     images: [
       Post02_Image01,
@@ -70,104 +70,104 @@ const posts = [
     ],
   },
   {
-    _id: 3,
+    id: 3,
     title: 'Portfólio',
     name: 'Dashboard Design',
-    descripion:
+    description:
       'Alguns meses após a finalização do 2º semestre da faculdade de Análise e Desenvolvimento de Sistemas, e com maiores conhecimentos de design, decidi fazer o redesign de algumas telas do nosso projeto entregue: Uma dashboard para monitoria de hardware do computador.',
     images: [Post03_Image01, Post03_Image02],
   },
   {
-    _id: 4,
+    id: 4,
     title: 'Logo Design',
     name: 'Identidade Visual',
-    descripion:
+    description:
       'Projetos de Identidade Visual certamente estão bem distantes de estar em minhas hard skills. Entretanto, no 4º semestre da faculdade, foi-me passado um desafio para desenvolver uma identidade e um design à um app de turismos; nesse processo surgiu o Straway.',
     images: [Post04_Image01],
   },
   {
-    _id: 5,
+    id: 5,
     title: 'Website Creation',
     name: 'devs.nation',
-    descripion:
+    description:
       'A devs.nation foi uma proposta de startup realizada pelo @danilodonato onde fui responsável de desenvolver do total zero uma landing page e a estrutura inicial do branding de marca da companhia.',
     previewURL:
       'https://www.figma.com/proto/E4M9qWLPmC20gb8MPYlvbe/devs.nation?node-id=10%3A114&scaling=min-zoom',
     images: [Post05_Image01, Post05_Image02, Post05_Image03, Post05_Image04],
   },
   {
-    _id: 6,
+    id: 6,
     title: 'Portfólio',
     name: 'Developer Website',
-    descripion:
+    description:
       'No decorrer da minha carreira encontrei vários desenvolvedores que diziam "ter dificuldades com design" e, por este motivo, acavam não possuindo um portfólio. A partir disso pensei como seria um possível "template default" para desenvolvedores e criei uma landing page relativamente curta abordando seções básicas.',
     previewURL:
       'https://www.figma.com/proto/861RVvPnPX8agdimDNFI0s/Developer-Landing?node-id=36%3A6&scaling=min-zoom',
     images: [Post06_Image01, Post06_Image02, Post06_Image03],
   },
   {
-    _id: 7,
+    id: 7,
     title: 'Redesign Concept',
     name: 'Grupo Zap',
-    descripion:
+    description:
       'Neste estudo a minha proposta foi desenvolver a primeira dobra da landing page da companhia, buscando apresentar os principais call to actions ao usuário.',
     images: [Post07_Image01, Post07_Image02, Post07_Image03],
   },
   {
-    _id: 8,
+    id: 8,
     title: 'Portfólio',
     name: 'Rocketseat + GitHub',
-    descripion:
+    description:
       'Um projeto com requisitos mais técnicos, onde a proposta foi juntar a API da Rocketseat (disponível publicamente no curso Starter) e a API pública do GitHub para construir uma dashboard de gerenciamento de repositórios.',
     previewURL:
       'https://www.figma.com/proto/V3FyNCQU3B6gwxLqXbPlZK/React-Projects?node-id=43%3A49&scaling=min-zoom',
     images: [Post08_Image01, Post08_Image02, Post08_Image03],
   },
   {
-    _id: 9,
+    id: 9,
     title: 'Challenge Redesign',
     name: 'Portfólio - @semantixbr',
-    descripion:
+    description:
       'O teste para ingressar como Desenvolvedor Front-end na @semantixbr consistia em consumir uma Mocked API para apresentar gráficos de três maneiras diferentes (Pizza, Barras e Linhas) de acordo com os dados obtidos. Entretanto ao mesmo tempo em que estava elaborando o desafio também pensei que ele poderia ser um pouco diferente, visando principalmente uma maneira mais fácil de validar o nível de conhecimento dos desenvolvedores.',
     previewURL:
       'https://www.figma.com/proto/iCBQzbicigWsEsNBrbsowE/Semantix-Front-end-Test---Redesign?node-id=955%3A0&scaling=min-zoom',
     images: [Post09_Image01, Post09_Image02, Post09_Image03],
   },
   {
-    _id: 10,
+    id: 10,
     title: 'Portfólio Redesign',
     name: 'Danilo Donato',
-    descripion:
+    description:
       'Durante o último mês do ano passado (Dez. de 2.019) um dos meus amigos comentou que estava com planos para publicar um portfólio online mostrando os seus projetos desenvolvidos e também uma autoapresentação ao mercado.',
     previewURL:
       'https://www.figma.com/proto/4ZDftsi7oTFz0nAmUEhrtm/Danilo-Donato?node-id=10%3A38&scaling=min-zoom',
     images: [Post10_Image01, Post10_Image02, Post10_Image03],
   },
   {
-    _id: 11,
+    id: 11,
     title: 'Website Creation',
     name: 'Peeh Personal',
-    descripion:
+    description:
       'Uma landing page relativamente enxuta mostrando um pouco das principais skills de um Personal Trainer em conjunto com uma página de contato (onde busquei inserir boas práticas de design e acessibilidade em formulários) para possíveis agendamentos.',
     previewURL:
       'https://www.figma.com/proto/iR5brZGsnkNGhlZIWkvj4V/Pedro-Henrique?node-id=23%3A11&scaling=min-zoom',
     images: [Post11_Image01, Post11_Image02],
   },
   {
-    _id: 12,
+    id: 12,
     title: 'Redesign Concept',
     name: 'Zup Innovation',
-    descripion:
+    description:
       'Neste estudo busquei inspirações que me auxiliassem no desenvolvimento de uma interface clean que mostrasse as principais informações da companhia (como produtos, depoimentos, entre outros) em poucos scrolls do usuário. Para isso segui meu fluxo padrão de construção de interfaces: Coleta de Referências; Rabiscroframe; Branding Book; Wireframe; e Protótipo de Alta Fidelidade (o dito "design" em si).',
     previewURL:
       'https://www.figma.com/proto/lDHfxNI2VwrRcGXvBZ4OOw/Redesign%3A-Zup-Innovation?node-id=35%3A82&scaling=min-zoom',
     images: [Post12_Image01, Post12_Image02, Post12_Image03, Post12_Image04],
   },
   {
-    _id: 13,
+    id: 13,
     title: 'Redesign Concept',
     name: 'Vulpi App',
-    descripion:
+    description:
       'Navegando pelas postagens do LinkedIn encontrei vários profissionais divulgando seus perfis através da aplicação da Vulpi (uma empresa que oferece um serviço de recrutamento e indicação de profissionais de TI à outras companhias) e, através disto, vi uma boa oportunidade de colocar em prática alguns dos conhecimentos adquiridos no treinamento de design uiBoost.',
     previewURL:
       'https://www.figma.com/proto/smAb0iz8gEovBBDL0ydDdU/Redesign---Vulpi-App?node-id=25%3A31&scaling=min-zoom',
@@ -180,10 +180,10 @@ const posts = [
     ],
   },
   {
-    _id: 14,
+    id: 14,
     title: 'Website Creation',
     name: 'Carlos Eduardo',
-    descripion:
+    description:
       'Em Janeiro de 2.020 o @carlospandolf comentou sobre estar querendo desenvolver um portfólio para mostrar alguns dos seus projetos. A partir disso segui o meu processo padrão para estruturação de layouts (coleta de referências, wireframing, ...) em conjunto de um alinhamento de expectativas e desejos para construir uma landing page que fosse aderente às necessidades.',
     previewURL:
       'https://www.figma.com/proto/hHi5eDCRjsbDIsiX365uTH/Cadu-Ferreira?node-id=19%3A124&scaling=min-zoom',
