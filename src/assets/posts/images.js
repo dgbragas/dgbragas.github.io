248cbe033fa35d7791683f994ac5a8bc72a43c1a
dgbragas <dgbragas.dev@outlook.com>
2020-08-09 18:07:49 -0300

    Creating mocked posts data

---
+++ src/assets/posts/images.js
@@ -0,0 +1,104 @@
+import Post01_Image01 from './posts/01-post/image-01.png';
+import Post01_Image02 from './posts/01-post/image-02.png';
+import Post01_Image03 from './posts/01-post/image-03.png';
+
+import Post02_Image01 from './posts/02-post/image-01.png';
+import Post02_Image02 from './posts/02-post/image-02.png';
+import Post02_Image03 from './posts/02-post/image-03.png';
+import Post02_Image04 from './posts/02-post/image-04.png';
+import Post02_Image05 from './posts/02-post/image-05.png';
+
+import Post03_Image01 from './posts/03-post/image-01.png';
+import Post03_Image02 from './posts/03-post/image-02.png';
+
+import Post04_Image01 from './posts/04-post/image-01.png';
+
+import Post05_Image01 from './posts/05-post/image-01.png';
+import Post05_Image02 from './posts/05-post/image-02.png';
+import Post05_Image03 from './posts/05-post/image-03.png';
+import Post05_Image04 from './posts/05-post/image-04.png';
+
+import Post06_Image01 from './posts/06-post/image-01.png';
+import Post06_Image02 from './posts/06-post/image-02.png';
+import Post06_Image03 from './posts/06-post/image-03.png';
+
+import Post07_Image01 from './posts/07-post/image-01.png';
+import Post07_Image02 from './posts/07-post/image-02.png';
+import Post07_Image03 from './posts/07-post/image-03.png';
+
+import Post08_Image01 from './posts/08-post/image-01.png';
+import Post08_Image02 from './posts/08-post/image-02.png';
+import Post08_Image03 from './posts/08-post/image-03.png';
+
+import Post09_Image01 from './posts/09-post/image-01.png';
+import Post09_Image02 from './posts/09-post/image-02.png';
+import Post09_Image03 from './posts/09-post/image-03.png';
+
+import Post10_Image01 from './posts/10-post/image-01.png';
+import Post10_Image02 from './posts/10-post/image-02.png';
+import Post10_Image03 from './posts/10-post/image-03.png';
+
+import Post11_Image01 from './posts/11-post/image-01.png';
+import Post11_Image02 from './posts/11-post/image-02.png';
+
+import Post12_Image01 from './posts/12-post/image-01.png';
+import Post12_Image02 from './posts/12-post/image-02.png';
+import Post12_Image03 from './posts/12-post/image-03.png';
+import Post12_Image04 from './posts/12-post/image-04.png';
+
+import Post13_Image01 from './posts/13-post/image-01.png';
+import Post13_Image02 from './posts/13-post/image-02.png';
+import Post13_Image03 from './posts/13-post/image-03.png';
+import Post13_Image04 from './posts/13-post/image-04.png';
+import Post13_Image05 from './posts/13-post/image-05.png';
+
+import Post14_Image01 from './posts/14-post/image-01.png';
+import Post14_Image02 from './posts/14-post/image-02.png';
+import Post14_Image03 from './posts/14-post/image-03.png';
+
+export {
+  Post01_Image01,
+  Post01_Image02,
+  Post01_Image03,
+  Post02_Image01,
+  Post02_Image02,
+  Post02_Image03,
+  Post02_Image04,
+  Post02_Image05,
+  Post03_Image01,
+  Post03_Image02,
+  Post04_Image01,
+  Post05_Image01,
+  Post05_Image02,
+  Post05_Image03,
+  Post05_Image04,
+  Post06_Image01,
+  Post06_Image02,
+  Post06_Image03,
+  Post07_Image01,
+  Post07_Image02,
+  Post07_Image03,
+  Post08_Image01,
+  Post08_Image02,
+  Post08_Image03,
+  Post09_Image01,
+  Post09_Image02,
+  Post09_Image03,
+  Post10_Image01,
+  Post10_Image02,
+  Post10_Image03,
+  Post11_Image01,
+  Post11_Image02,
+  Post12_Image01,
+  Post12_Image02,
+  Post12_Image03,
+  Post12_Image04,
+  Post13_Image01,
+  Post13_Image02,
+  Post13_Image03,
+  Post13_Image04,
+  Post13_Image05,
+  Post14_Image01,
+  Post14_Image02,
+  Post14_Image03,
+};
