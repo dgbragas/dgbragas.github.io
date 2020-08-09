b9596bbed0eb1739a62de7f023da97e6b2162a56
dgbragas <dgbragas.dev@outlook.com>
2020-08-09 19:00:26 -0300

    Integrating <ViewProjectPage />

--- src/pages/ViewProject/index.tsx
+++ src/pages/ViewProject/index.tsx
@@ -5,33 +5,38 @@ import { ReactComponent as ArrowIcon } from '../../assets/icons/button-arrow.svg
 
 import DefaultLayout from '../../layouts/Default';
 import Slider from '../../components/Slider';
+
 import * as S from './styles';
+import If from '../../components/If';
+
+interface StateProps {
+  full_description: string;
+  images: string[];
+  title: string;
+  previewURL?: string;
+}
 
 const ViewProject: React.FC = () => {
-  const { state } = useLocation();
-
-  console.log(state);
+  const { state } = useLocation<StateProps>();
 
   return (
     <DefaultLayout>
       <S.Wrapper>
         <S.ProjectSlide>
-          <Slider />
+          <Slider images={state.images} />
         </S.ProjectSlide>
 
         <S.ProjectInfos>
-          <S.Title>Danilo Donato</S.Title>
-          <S.Description>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
-            purus sit amet luctus venenatis, lectus magna fringilla urna,
-            porttitor rhoncus dolor purus non enim praesent.
-          </S.Description>
-          <S.Preview>
-            <div>
-              <ArrowIcon />
-            </div>
-            Protótipo navegável
-          </S.Preview>
+          <S.Title>{state.title}</S.Title>
+          <S.Description>{state.full_description}</S.Description>
+          <If condition={!!state.previewURL}>
+            <S.Preview href={state.previewURL}>
+              <div>
+                <ArrowIcon />
+              </div>
+              Protótipo navegável
+            </S.Preview>
+          </If>
         </S.ProjectInfos>
       </S.Wrapper>
     </DefaultLayout>
