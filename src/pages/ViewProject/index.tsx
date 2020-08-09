83e28e9835739aa1d88f1db69a0b91bdb429681c
dgbragas <dgbragas.dev@outlook.com>
2020-08-09 16:38:15 -0300

    Initial <ViewProjectPage /> layout structure

--- src/pages/ViewProject/index.tsx
+++ src/pages/ViewProject/index.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
+import { ReactComponent as ArrowIcon } from '../../assets/icons/button-arrow.svg';
+
+import DefaultLayout from '../../layouts/Default';
+import Slider from '../../components/Slider';
 import * as S from './styles';
 
-const ViewProject: React.FC = () => <div />;
+const ViewProject: React.FC = () => {
+  const { state } = useLocation();
+
+  console.log(state);
+
+  return (
+    <DefaultLayout>
+      <S.Wrapper>
+        <S.ProjectSlide>
+          <Slider />
+        </S.ProjectSlide>
+
+        <S.ProjectInfos>
+          <S.Title>Danilo Donato</S.Title>
+          <S.Description>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna,
+            porttitor rhoncus dolor purus non enim praesent.
+          </S.Description>
+          <S.Preview>
+            <div>
+              <ArrowIcon />
+            </div>
+            Protótipo navegável
+          </S.Preview>
+        </S.ProjectInfos>
+      </S.Wrapper>
+    </DefaultLayout>
+  );
+};
 
 export default ViewProject;
--- src/pages/ViewProject/styles.ts
+++ src/pages/ViewProject/styles.ts
@@ -1,3 +1,77 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Wrapper = styled.section`
+  display: grid;
+  grid-gap: 32px;
+  grid-template-columns: 1.5fr 1fr;
+  padding: 104px 0 128px;
+`;
+
+export const ProjectSlide = styled.div`
+  height: 700px;
+  width: 100%;
+`;
+
+export const ProjectInfos = styled.div`
+  border: 1px solid rgba(252, 252, 252, 0.1);
+  border-radius: 10px;
+  height: fit-content;
+  padding: 64px;
+`;
+
+export const Title = styled.h1`
+  color: ${({ theme: { colors } }) => colors.secondary};
+  background: linear-gradient(238.86deg, #fa9401 15.27%, #ef3f36 96.96%) 100%
+    no-repeat;
+  font-size: 32px;
+  line-height: 133%;
+  margin-bottom: 13px;
+
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -moz-background-clip: text;
+  -moz-text-fill-color: transparent;
+`;
+
+export const Description = styled.p`
+  color: ${({ theme: { colors } }) => colors.texts};
+  line-height: 180%;
+  margin-bottom: 40px;
+  opacity: 0.8;
+`;
+
+export const Preview = styled.a`
+  cursor: pointer;
+  align-items: center;
+  color: ${({ theme: { colors } }) => colors.texts};
+  display: flex;
+  font-weight: 600;
+
+  div {
+    align-items: center;
+    background-color: ${({ theme: { colors } }) => colors.texts};
+    border-radius: 50%;
+    display: flex;
+    height: 40px;
+    justify-content: center;
+    margin-right: 16px;
+    transition: background 0.3s ease;
+    width: 40px;
+
+    svg path {
+      transition: fill 0.3s ease;
+    }
+  }
+
+  &:hover {
+    text-decoration: underline;
+
+    div {
+      background: linear-gradient(238.86deg, #fa9401 15.27%, #ef3f36 96.96%);
+    }
+
+    svg path {
+      fill: ${({ theme: { colors } }) => colors.texts};
+    }
+  }
+`;
